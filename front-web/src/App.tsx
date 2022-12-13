93797066549edbcbc0ae18a1ef60d1fde50a2ced
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2022-12-13 07:48:53 -0300

    Added Filter component

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './components/header';
 import './App.css';
+import Filter from './components/filter';
 
 function App() {
   return (
     <>
       <Header />
+      <div className="app-container">
+        <Filter />
+      </div>
     </>
   );
 }

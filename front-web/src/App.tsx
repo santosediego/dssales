3acc0aef9e1b695093176a9b840ae29261ff95d9
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2022-12-13 11:08:25 -0300

    Added PieChartCard component

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Header from './components/header';
-import './App.css';
 import Filter from './components/filter';
 import SalesByDate from './components/sales-by-date';
 import SalesSummary from './components/sales-summary';
+import PieChartCard from './components/pie-chart-card';
+import './App.css';
 
 function App() {
   return (
@@ -14,6 +15,16 @@ function App() {
         <SalesByDate />
         <div className="sales-overview-container">
           <SalesSummary />
+          <PieChartCard
+            name="Lojas"
+            labels={['Uberlândia', 'Araguai', 'Uberaba']}
+            series={[40, 30, 30]}
+          />
+          <PieChartCard
+            name="Pagamento"
+            labels={['Crédito', 'Débito', 'Dinheiro']}
+            series={[50, 30, 20]}
+          />
         </div>
       </div>
     </>

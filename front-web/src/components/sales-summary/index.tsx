0d6313dec97b8a20d1c4c05479e6c9e1fe06d715
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2022-12-13 16:37:32 -0300

    SalesByDate and SalesSummary integration with the application

--- front-web/src/components/sales-summary/index.tsx
+++ front-web/src/components/sales-summary/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import SalesSummaryCard from './sales-summary-card';
+import { FilterData, SalesSummaryData } from '../../types';
+import { buildFilterParams, makeRequest } from '../../utils/request';
 import './styles.css';
 
 import { ReactComponent as AvatarIcon } from '../../assets/avatar-icon.svg';
@@ -7,13 +9,39 @@ import { ReactComponent as BarCharIcon } from '../../assets/bar-chart-icon.svg';
 import { ReactComponent as DoneIcon } from '../../assets/done-icon.svg';
 import { ReactComponent as SyncIcon } from '../../assets/sync-icon.svg';
 
-function SalesSummary() {
+type Props = {
+  filterData?: FilterData;
+};
+
+const initialSumary = {
+  avg: 0,
+  count: 0,
+  max: 0,
+  min: 0
+};
+
+function SalesSummary({ filterData }: Props) {
+  const [summary, setSummary] = useState<SalesSummaryData>(initialSumary);
+
+  const params = useMemo(() => buildFilterParams(filterData), [filterData]);
+
+  useEffect(() => {
+    makeRequest
+      .get<SalesSummaryData>(`/sales/summary`, { params })
+      .then((response) => {
+        setSummary(response.data);
+      })
+      .catch(() => {
+        console.error('Error to fetch sales by date');
+      });
+  }, [params]);
+
   return (
     <div className="sales-summary-container">
-      <SalesSummaryCard value={534} label="Média" icon={<DoneIcon />} />
-      <SalesSummaryCard value={44434} label="Quantidade" icon={<SyncIcon />} />
-      <SalesSummaryCard value={434} label="Mínima" icon={<BarCharIcon />} />
-      <SalesSummaryCard value={3434} label="Máxima" icon={<AvatarIcon />} />
+      <SalesSummaryCard value={summary?.avg?.toFixed(2)} label="Média" icon={<DoneIcon />} />
+      <SalesSummaryCard value={summary?.count} label="Quantidade" icon={<SyncIcon />} />
+      <SalesSummaryCard value={summary?.min} label="Mínima" icon={<BarCharIcon />} />
+      <SalesSummaryCard value={summary?.max} label="Máxima" icon={<AvatarIcon />} />
     </div>
   );
 }
